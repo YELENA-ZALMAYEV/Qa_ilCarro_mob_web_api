9cb3238357a83e156e81ad5334c4a7158b2263b5
YELENA-ZALMAYEV <elenkina73@gmail.com>
2024-08-21 13:54:24 +0300

    new project

---
+++ src/main/java/screens/SearchScreen.java
@@ -0,0 +1,10 @@
+package screens;
+
+import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.android.AndroidElement;
+
+public class SearchScreen extends BaseScreen{
+    public SearchScreen(AppiumDriver<AndroidElement> driver) {
+        super(driver);
+    }
+}
